48703463eaf7b0a8fb81af9cc24215016eaf1006
myslak71 <Zabojca2golebi>
2019-01-28 18:13:56 +0100

    update readme and travis

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import React from "react";
 import {Route} from "react-router-dom";
 
 import DuelDetail from "./containers/DuelDetailView"
-import ArticleList from "./containers/ArticleListView";
-import ArticleDetail from "./containers/ArticleDetailView";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import DuelList from "./containers/DuelListView";
@@ -11,7 +9,6 @@ import DuelList from "./containers/DuelListView";
 const BaseRouter = () => (
     <div>
         <Route exact path="/" component={DuelList}/>{" "}
-        <Route exact path="/articles/:articleID/" component={ArticleDetail}/>{" "}
         <Route exact path="/login/" component={Login}/>{" "}
         <Route exact path="/signup/" component={Signup}/>{" "}
         <Route exact path="/duel/" component={DuelList}/>{" "}
