a926c4b2244d76407fe17e41b4f51114ae30f267
myslak71 <Zabojca2golebi>
2019-02-10 15:10:31 +0100

    add test_duel_create_view_valid

--- duels/tests/test_utils.py
+++ duels/tests/test_utils.py
@@ -8,16 +8,14 @@ from duels.models import Duel, Dataset, Algorithm
 
 class TestUtils(unittest.TestCase):
     def setUp(self):
-        user1 = User.objects.create_user(username='user1', email='user1@a.com', password='user1')
-        user2 = User.objects.create_user(username='user2', email='user1@a.com', password='user2')
-        dataset = Dataset.objects.create(name='test_dataset', dataset='datasets/test.csv')
-        algorithm = Algorithm.objects.create(name='0', parameters={'n_neighbors': 1})
-        duel = Duel.objects.create(user1=user1, user2=user2, dataset=dataset)
-        duel.rounds.add(algorithm)
+        self.user1 = User.objects.create_user(username='user1', email='user1@a.com', password='user1')
+        self.user2 = User.objects.create_user(username='user2', email='user1@a.com', password='user2')
+        self.dataset = Dataset.objects.create(name='test_dataset', dataset='datasets/test.csv')
+        self.algorithm = Algorithm.objects.create(name='0', parameters={'n_neighbors': 1})
+        self.duel = Duel.objects.create(user1=self.user1, user2=self.user2, dataset=self.dataset)
+        self.duel.rounds.add(self.algorithm)
 
     def test_proper_value(self):
-        duel = Duel.objects.get(pk=1)
-        algorithm = Algorithm.objects.get(pk=1)
-        self.assertEqual(count_percentage(duel, algorithm), 99.074)
+        self.assertEqual(count_percentage(self.duel, self.algorithm), 99.074)
 
 
